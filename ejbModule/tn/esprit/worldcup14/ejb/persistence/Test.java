dc0c9c362bca96730edd6436ac71d06695bdd851
seifboujemaa <seifeddine.boujemaa@esprit.tn>
2013-10-01 20:47:27 +0100

    commit 1

---
+++ ejbModule/tn/esprit/worldcup14/ejb/persistence/Test.java
@@ -0,0 +1,17 @@
+package tn.esprit.worldcup14.ejb.persistence;
+
+public class Test {
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
